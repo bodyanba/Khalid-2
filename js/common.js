373799c98a1dd0e1e2e6ef3c0f3c7be2521f5bd3
bodyanba <galkinbogdan@gmail.com>
2019-06-11 14:50:09 +0300

    animations

--- js/common.js
+++ js/common.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
   $(".owl-1").owlCarousel({
     items: 1,
     smartSpeed: 1000,
+    loop: true,
+    autoplay: true,
   });
 
   $(".navbar-toggler").on("click", function() {
@@ -10,5 +12,12 @@ $(document).ready(function () {
   });
 
   $(window).enllax();
+
+  $('.jarallax').jarallax({});
+
+  AOS.init({
+    duration: 1500,
+    disable: 'mobile'
+  });
 
 });
